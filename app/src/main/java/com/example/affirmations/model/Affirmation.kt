b3ccb681daad5b8a4d202e6a7be957f81ecfdcae
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-04-19 16:34:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,10 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringRecursId: Int,
+    @DrawableRes val imageRecursId: Int,
+)
+
